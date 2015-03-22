4002db96368a88c882a0dc749518b7a1dc605a26
Arvi Acuzar <arvi.acuzar@gmail.com>
2015-03-22 12:30:38 +0800

    added: support for autoform 5.0

--- package.js
+++ package.js
@@ -1,15 +1,20 @@
 Package.describe({
   name: 'comerc:autoform-typeahead',
   summary: 'Custom "typeahead" input type for AutoForm',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/comerc/meteor-autoform-typeahead.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('templating@1.0.0');
-  api.use('blaze@2.0.0');
-  api.use('aldeed:autoform@4.0.0');
+
+  api.use([
+    'templating',
+    'blaze',
+    'aldeed:autoform',
+    'comerc:bs-typeahead'
+  ]);
+
   api.addFiles([
     'autoform-typeahead.html',
     'autoform-typeahead.js',
